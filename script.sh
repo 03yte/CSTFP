b567f017ff995c9ffc6ea9c25ae6054160b62ea9
03yte <adrianbugnacki@gmail.com>
2022-01-24 14:47:18 -0500

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,13 @@
+#Dependencies
+apt install 
+#Acquire Files
+cd /tmp
+git clone https://github.com/03yte/CSTFP
+cd /tmp/CSTFP
+cp /tmp/CSTFP/printer.service /etc/systemd/service/
+
+
+
+#Start services
+systemctl enable printer.service
+systemctl start printer.service
