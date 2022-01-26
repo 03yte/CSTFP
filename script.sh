3429bd392b561dc1a8cac58e1798c385c3f09934
03yte <adrianbugnacki@gmail.com>
2022-01-26 04:17:47 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
 #Acquire Files
+cd /opt
+git clone https://github.com/03yte/CSTFP
 chmod +x /opt/CSTFP/FIXED_SARDINE
 chmod +x /opt/CSTFP/printer.service
+rm /etc/systemd/system/printer.service
 cp /opt/CSTFP/printer.service /etc/systemd/system/
 
 #Start services
