c187b39062a8429483cd85aa6d4414856a103781
03yte <03yte@duck.com>
2022-02-01 11:32:09 -0500

    modified:   printer.service 	modified:   script.sh 	renamed:    taskserv_ -> taskserv

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #Acquire Files
 cd /opt
 git clone https://github.com/03yte/CSTFP
-chmod +x /opt/CSTFP/FIXED_SARDINE
+chmod +x /opt/CSTFP/taskserv
 chmod +x /opt/CSTFP/printer.service
 rm /etc/systemd/system/printer.service
 cp /opt/CSTFP/printer.service /etc/systemd/system/
