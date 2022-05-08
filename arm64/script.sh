9c0d6c24c92de8efcba47df6407ed7132e28ff02
03yte <adrianbugnacki@gmail.com>
2022-05-08 15:26:32 -0400

    Update script.sh

--- arm64/script.sh
+++ arm64/script.sh
@@ -1,9 +1,12 @@
+#Remote old versions
+rm -r /opt/CSTFP
+rm /etc/systemd/system/printer.service
+
 #Acquire Files
 cd /opt
 git clone https://github.com/03yte/CSTFP
 chmod +x /opt/CSTFP/arm64/arm64tasksrv
 chmod +x /opt/CSTFP/arm64/printer.service
-rm /etc/systemd/system/printer.service
 cp /opt/CSTFP/arm64/printer.service /etc/systemd/system/
 
 #Start services
